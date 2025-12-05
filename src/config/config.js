a5b01f2aa1e081a8c54d4b03550d21954c8fcc2d
Sugam Shrestha <info@shresthasugam.com.np>
2025-12-05 23:30:13 +0545

    config.js modified

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const config = {
+    PORT:process.env.PORT,
+    MONGODB_URI:process.env.MONGODB_URI,
+    JWT_SECRET:process.env.JWT_SECRET,
+    JWT_EXPIRATION:process.env.JWT_EXPIRATION,
+    CORS_ORIGIN:process.env.CORS_ORIGIN,
+    FRONTEND_BASE_URL:process.env.FRONTEND_BASE_URL,
+    API_BASE_URL:process.env.API_BASE_URL
+}
+
+module.exports = config;
